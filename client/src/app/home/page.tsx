5f760bde0295252513c7cf1b0aaeb4381f7e462a
MD.Shimul <md.shimuldiu@gmail.com>
2026-08-26 02:42:56 +0600

    refactor: remove AllActivities component import from HomePage

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Banner from './banner/page';
 import QuickDonationCard from './QuickDonationCard/page';
-import AllActivities from '../component/Allactivity/page';
 
 const HomePage = () => {
   return (
@@ -13,7 +12,7 @@ const HomePage = () => {
       {/* QuickDonationCard-কে সব সময় Horizontally Center রাখার জন্য */}
       <div className='absolute top-3/4 sm:top-11/12 left-1/2 -translate-x-1/2 w-11/12 md:w-10/12 z-20'>
         <QuickDonationCard />
-        
+
       </div>
     </div>
   );
